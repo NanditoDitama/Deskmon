18c62c1266558ee4b9aeaef21d1d675eeab73950
Daniel Aprillio <70996943+Nielioo@users.noreply.github.com>
2025-07-28 11:21:23 +0700

    Build for ios

--- build/Desktop_Qt_6_9_0_MinGW_64_bit-Release/.qt/Deskmon_res.cmake
+++
@@ -1,7 +0,0 @@
-
-set(target "Deskmon")
-set(working_dir "C:/Users/prnl2/OneDrive/Documents/window_logger")
-set(src_and_dest_list
-
-)
-set(timestamp_file "C:/Users/prnl2/OneDrive/Documents/window_logger/build/Desktop_Qt_6_9_0_MinGW_64_bit-Release/.qt/Deskmon_res.txt")
